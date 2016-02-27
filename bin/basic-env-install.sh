045f11c84a4a6d2b4487f292998e1ffa7f815c17
Varun <varunkatta@gmail.com>
2016-02-27 15:13:34 -0800

    added a new script.

---
+++ bin/basic-env-install.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+cd $HOME
+sudo yum -y install zsh git vim
+git clone git://github.com/varunkatta/oh-my-zsh.git .oh-my-zsh
+ln -s .oh-my-zsh/templates/zshrc-varun .zshrc
+zsh
+cat zsh >> $HOME/.bashrc
+ls
